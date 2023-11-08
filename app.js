0a72cc2d7e1bca340a2b085fc80db8d3837938b2
Bonaudo <93019653+Bonaudo@users.noreply.github.com>
2023-11-08 17:35:01 -0300

    formulario terminado

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
 function validarFormulario(event) {
-    var nombre = document.getElementById("Nombre").value;
-    var correo = document.getElementById("mail").value;
-    var edad = document.getElementById("Edad").value;
-    var telefono = document.getElementById("Telefono").value;
-    var mensaje = document.getElementById("Mensaje").value;
+    var nombre = document.getElementById("Nombre");
+    var correo = document.getElementById("mail");
+    var edad = document.getElementById("Edad");
+    var telefono = document.getElementById("Telefono");
+    var mensaje = document.getElementById("Mensaje");
     var errorMensaje = document.getElementById("error-message");
 
     // Validar que en las áreas de texto solo se ingresen letras
-    if (!contieneSoloLetras(nombre)) {
-        document.getElementById("Nombre").style.backgroundColor = "lightcoral";
+    if (!contieneSoloLetras(nombre.value)) {
+        nombre.style.backgroundColor = "lightcoral";
         event.preventDefault(); // Evitar el envío si hay errores
-        return false;
     } else {
-        document.getElementById("Nombre").style.backgroundColor = "white";
+        nombre.style.backgroundColor = "white";
     }
 
     // Validar que en las áreas de números solo se ingresen números
-    if (!contieneSoloNumeros(edad) || !contieneSoloNumeros(telefono)) {
-        document.getElementById("Edad").style.backgroundColor = "lightcoral";
-        document.getElementById("Telefono").style.backgroundColor = "lightcoral";
+    if (!contieneSoloNumeros(edad.value) || !contieneSoloNumeros(telefono.value)) {
+        edad.style.backgroundColor = "lightcoral";
+        telefono.style.backgroundColor = "lightcoral";
         event.preventDefault(); // Evitar el envío si hay errores
-        return false;
     } else {
-        document.getElementById("Edad").style.backgroundColor = "white";
-        document.getElementById("Telefono").style.backgroundColor = "white";
+        edad.style.backgroundColor = "white";
+        telefono.style.backgroundColor = "white";
+    }
+
+    // Validar el correo electrónico manualmente
+    if (!validarCorreo(correo.value)) {
+        correo.style.backgroundColor = "lightcoral";
+        event.preventDefault(); // Evitar el envío si hay errores
+    } else {
+        correo.style.backgroundColor = "white";
+    }
+
+    // Validar si el campo de mensaje está vacío y mostrar el mensaje de error
+    if (mensaje.value === "") {
+        mensaje.style.backgroundColor = "lightcoral";
+        event.preventDefault(); // Evitar el envío si hay errores
+    } else {
+        mensaje.style.backgroundColor = "white";
     }
 
     // Validar si algún campo está vacío y mostrar el mensaje de error
-    if (nombre === "" || correo === "" || edad === "" || telefono === "" || mensaje === "") {
+    if (nombre.value === "" || correo.value === "" || edad.value === "" || telefono.value === "" || mensaje.value === "") {
         errorMensaje.style.display = "block";
         event.preventDefault(); // Evitar el envío si hay errores
-        return false;
     } else {
         // Si todos los campos son válidos, ocultar el mensaje de error
         errorMensaje.style.display = "none";
@@ -43,4 +56,9 @@ function contieneSoloLetras(texto) {
 
 function contieneSoloNumeros(texto) {
     return /^[0-9]+$/.test(texto);
+}
+
+function validarCorreo(correo) {
+    var regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regexCorreo.test(correo);
 }
