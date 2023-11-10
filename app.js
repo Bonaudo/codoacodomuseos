015378d523b47beb2efb4d52de7511911721696c
Bonaudo <93019653+Bonaudo@users.noreply.github.com>
2023-11-10 14:23:57 -0300

    js

--- app.js
+++ app.js
@@ -6,46 +6,46 @@ function validarFormulario(event) {
     var mensaje = document.getElementById("Mensaje");
     var errorMensaje = document.getElementById("error-message");
 
-    // Validar que en las áreas de texto solo se ingresen letras
+    
     if (!contieneSoloLetras(nombre.value)) {
         nombre.style.backgroundColor = "lightcoral";
-        event.preventDefault(); // Evitar el envío si hay errores
+        event.preventDefault(); 
     } else {
         nombre.style.backgroundColor = "white";
     }
 
-    // Validar que en las áreas de números solo se ingresen números
+    
     if (!contieneSoloNumeros(edad.value) || !contieneSoloNumeros(telefono.value)) {
         edad.style.backgroundColor = "lightcoral";
         telefono.style.backgroundColor = "lightcoral";
-        event.preventDefault(); // Evitar el envío si hay errores
+        event.preventDefault(); 
     } else {
         edad.style.backgroundColor = "white";
         telefono.style.backgroundColor = "white";
     }
 
-    // Validar el correo electrónico manualmente
+    
     if (!validarCorreo(correo.value)) {
         correo.style.backgroundColor = "lightcoral";
-        event.preventDefault(); // Evitar el envío si hay errores
+        event.preventDefault(); 
     } else {
         correo.style.backgroundColor = "white";
     }
 
-    // Validar si el campo de mensaje está vacío y mostrar el mensaje de error
+    
     if (mensaje.value === "") {
         mensaje.style.backgroundColor = "lightcoral";
-        event.preventDefault(); // Evitar el envío si hay errores
+        event.preventDefault(); 
     } else {
         mensaje.style.backgroundColor = "white";
     }
 
-    // Validar si algún campo está vacío y mostrar el mensaje de error
+    
     if (nombre.value === "" || correo.value === "" || edad.value === "" || telefono.value === "" || mensaje.value === "") {
         errorMensaje.style.display = "block";
-        event.preventDefault(); // Evitar el envío si hay errores
+        event.preventDefault(); 
     } else {
-        // Si todos los campos son válidos, ocultar el mensaje de error
+        
         errorMensaje.style.display = "none";
     }
 }
